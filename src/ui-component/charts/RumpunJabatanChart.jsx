ffcf91e1a3cb7a78f327f83ad6f075bd6cc4b1ab
ayuningsukarman <ayuningning19@gmail.com>
2024-04-24 17:07:33 +0700

    update chart

--- src/ui-component/charts/RumpunJabatanChart.jsx
+++ src/ui-component/charts/RumpunJabatanChart.jsx
@@ -1,115 +1,117 @@
-import PropTypes from 'prop-types';
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-
-// material-ui
-import { useTheme } from '@mui/material/styles';
-import { Grid, Typography } from '@mui/material';
-
-// third-party
+import React from 'react';
 import ApexCharts from 'apexcharts';
-import Chart from 'react-apexcharts';
-
-// project imports
-import SkeletonTotalGrowthBarChart from './RumpunJabatanChart00';
 import MainCard from '../../ui-component/cards/MainCard';
+import { Grid, Typography } from '@mui/material';
 import { gridSpacing } from '../../store/constant';
+import ButtonChart from '../../ui-component/button/ButtonChart';
 
-// chart data
-import chartData from '../../views/dashboard/Default/chart-data/total-growth-bar-chart';
+const RumpunJabatanChart = ({ series, options }) => {
+  const chartRef = React.createRef();
 
-// ==============================|| DASHBOARD DEFAULT - TOTAL GROWTH BAR CHART ||============================== //
-
-const RumpunJabatanChart = ({ isLoading }) => {
-  const theme = useTheme();
-  const customization = useSelector((state) => state.customization);
-
-  const { navType } = customization;
-  const { primary } = theme.palette.text;
-  const grey200 = theme.palette.grey[200];
-  const grey500 = theme.palette.grey[500];
-
-  useEffect(() => {
-    const newChartData = {
-      ...chartData.options,
+  React.useEffect(() => {
+    const chart = new ApexCharts(chartRef.current, {
+      ...options,
+      series,
       width: 300,
       height: 400,
-      colors: ['#0F1C3E'], // Mengubah warna grafik menjadi '#0F1C3E'
-      xaxis: {
-        labels: {
-          style: {
-            colors: ['#0F1C3E'] // Mengubah warna label sumbu x menjadi '#0F1C3E'
-          }
-        }
-      },
-      yaxis: {
-        labels: {
-          style: {
-            colors: [primary] // Menggunakan warna utama yang sudah ditentukan
-          }
-        }
-      },
-      grid: {
-        borderColor: grey200 // Menggunakan warna grid yang sudah ditentukan
-      },
-      tooltip: {
-        theme: 'light' // Menggunakan tema tooltip yang sudah ditentukan
-      },
-      legend: {
-        labels: {
-          colors: grey500 // Menggunakan warna label legenda yang sudah ditentukan
-        }
+    });
+
+    chart.render();
+
+    return () => {
+      if (chart) {
+        chart.destroy();
       }
     };
-
-    // do not load chart when loading
-    if (!isLoading) {
-      ApexCharts.exec(`bar-chart`, 'updateOptions', newChartData);
-    }
-  }, [navType, primary, grey200, grey500, isLoading]);
+  }, [series, options]);
 
   return (
-    <>
-      {isLoading ? (
-        <SkeletonTotalGrowthBarChart />
-      ) : (
-        <MainCard maxWidth="lg" >
-          <Grid container spacing={gridSpacing} paddingTop={3} paddingLeft={3} paddingRight={3}>
-            <Grid item xs={12} md={12}>
-              <Grid container alignItems="center" justifyContent="space-between">
-                <Grid item> 
-                  <Grid container direction="column" spacing={1}>
-                    <Grid item>
-                      <Typography variant="h3">Total Talent Berdasarkan Rumpun Jabatan</Typography>
-                    </Grid>
-                  </Grid>
+    <MainCard maxWidth="lg">
+      <Grid container spacing={gridSpacing} paddingTop={3} paddingLeft={3} paddingRight={3}>
+        <Grid item xs={12} md={12}>
+          <Grid container alignItems="center" justifyContent="space-between">
+            <Grid item> 
+              <Grid container direction="column" spacing={1}>
+                <Grid item>
+                  <Typography variant="h3">Proporsi Talent Berdasarkan Rumpun Jabatan </Typography>
                 </Grid>
               </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <Chart {...chartData} />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              {/* <Typography variant="body1">Keterangan</Typography>
-              <Typography variant="body2" textAlign="right"></Typography>
-              PP: Perencanaan dan Pengelolaan Strategis<br />
-              B  :  Bisnis<br/>
-              OP :  Operasi<br />
-              MR : Manajemen Resiko dan Kepatuhan<br />
-              PR : Pengelolaan Regulasi<br />
-              DI : Pengelolaan Teknologi<br />
-              KU : Keuangan<br />
-              SD : Sumber Daya Manusia<br /> */}
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <div ref={chartRef} />
+          <Grid item xs={12} md={6}>
+            <Typography variant="body1" style={{ fontWeight: 'bold', fontSize: '18px' }}>Keterangan</Typography>
+            {/* Button Grid */}
+            <Grid container spacing={1} justifyContent="start">
+              <Grid item xs={6} style={{marginRight:'12px'}}>
+                <ButtonChart buttonText="PP" detail="Perencanaan dan Pengelolaan Strategis" style={{marginRight:'12px'}}/>
+              </Grid>
+              <Grid item xs={6}>
+                <ButtonChart buttonText="B" detail="Bisnis" />
+              </Grid>
+              <Grid item xs={6}>
+                <ButtonChart buttonText="OP" detail="Operasi" />
+              </Grid>
+              <Grid item xs={6} style={{marginRight:'12px'}}>
+                <ButtonChart buttonText="MR" detail="Manajemen Resiko dan Kepatuhan" />
+              </Grid>
+              <Grid item xs={6} style={{marginRight:'12px'}}>
+                <ButtonChart buttonText="PR" detail="Pengelolaan Regulasi" />
+              </Grid>
+              <Grid item xs={6} style={{marginRight:'12px'}}>
+                <ButtonChart buttonText="DI" detail="Keuangan" />
+              </Grid>
+              <Grid item xs={6}>
+                <ButtonChart buttonText="SD" detail="Sumber Daya Manusia" />
+              </Grid>
             </Grid>
           </Grid>
-        </MainCard>
-      )}
-    </>
+        </Grid>
+      </Grid>
+    </MainCard>
   );
 };
 
-RumpunJabatanChart.propTypes = {
-  isLoading: PropTypes.bool
+RumpunJabatanChart.defaultProps = {
+  series: [
+    {
+      data: [50, 88, 118, 139, 151, 135, 98, 144],
+    },
+  ],
+  options: {
+    chart: {
+      type: 'bar',
+      height: 400,
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 1,
+        // Remove horizontal property to make the chart vertical
+        colors: {
+          ranges: [
+            { from: 1, to: 200, color: '#0F1C3E' },
+          ],
+        },
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      categories: [
+        'PP',
+        'B',
+        'OP',
+        'MR',
+        'PR',
+        'DI',
+        'KU',
+        'SD',
+      ],
+    },
+  },
 };
 
 export default RumpunJabatanChart;
