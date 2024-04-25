81409b0e134dc1c003e4ea1fb61394129ee5a50d
hannyros <156204730+hannyros@users.noreply.github.com>
2024-04-25 08:43:43 +0700

    tabel talent qualification

--- src/ui-component/tables/talentsource.jsx
+++ src/ui-component/tables/talentsource.jsx
@@ -1,36 +1,34 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import { idID } from '@mui/material/locale';
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: 'nama', headerName: 'Nama', width: 130 },
+  { field: 'nippos', headerName: 'Nippos', width: 130 },
+  { field: 'posisi', headerName: 'Posisi', width: 130 },
+  { field: 'joblevel', headerName: 'Job Level', width: 130 },
+  { field: 'rumpunjabatan', headerName: 'Rumpun Jabatan', width: 130 },
+  { field: 'kantor', headerName: 'Kantor', width: 130 },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
+    // field: 'age',
+    // headerName: 'Age',
+    // type: 'number',
+    // width: 90,
   },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-  },
+  // Menghapus definisi kolom ini jika tidak ada data yang relevan atau memperbaikinya
+  // {
+  //   field: 'fullName',
+  //   headerName: 'Full name',
+  //   description: 'This column has a value getter and is not sortable.',
+  //   sortable: false,
+  //   width: 160,
+  //   valueGetter: (params) => `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  // },
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, nama: 'Sri Hartini', nippos: '998294379', posisi: 'Asisten Manajer Pengembangan', joblevel: 'D3', rumpunjabatan: 'Bisnis', kantor: 'Kantor Pusat Bandung' },
 ];
 
 export default function TalentSource() {
