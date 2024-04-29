060b1b0619b14bdba8c0074994f775d8f5b2a7f5
farhanjuliansyahh <ahmadfarhan.juliansyah@gmail.com>
2024-04-29 15:54:54 +0700

    update layouting

--- src/views/pages/talent-identification/daftar-event.jsx
+++ src/views/pages/talent-identification/daftar-event.jsx
@@ -151,7 +151,7 @@ const DaftarEvent = () => {
           </Tabs>
         </Box>
 
-        <CustomTabPanel value={value} index={0}>
+        <CustomTabPanel value={value} index={0} style={{paddingLeft: '24px', paddingRight: '24px', marginBottom: '24px',}}>
         {isLoading ? (
           <Typography>Loading...</Typography>
         ) : eventData.length > 0 ? (
