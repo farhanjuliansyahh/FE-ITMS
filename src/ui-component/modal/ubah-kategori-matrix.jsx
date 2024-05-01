2fb1651e76d7ae5ae0dab64b53153bfca66dc752
blue-sky <105834394+hrtnisri2016@users.noreply.github.com>
2024-05-01 16:20:48 +0700

    add buttons and their logic di page ketua komite talent

---
+++ src/ui-component/modal/ubah-kategori-matrix.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { Dialog, DialogActions, DialogContent, DialogTitle, Box, Button, MenuItem, TextField, Typography, Divider } from '@mui/material';
+import { CancelOutlined, CreateOutlined } from '@mui/icons-material';
+import KonfirmasiUbahMatrix from './konfirmasi-ubah-matrix';
+
+function UbahKategoriMatrix({ open, onClose, onOpenSecondModal }) {
+    const [openKonfirmasiModal, setOpenKonfirmasiModal] = useState(false);
+
+    const handleOpenKonfirmasiModal = () => {
+        onClose(); // Close the current modal
+        setOpenKonfirmasiModal(true);
+    };
+
+    const handleCloseKonfirmasiModal = () => {
+        setOpenKonfirmasiModal(false);
+    };
+
+    const handleUbahMatrixButtonClick = (event) => {
+        event.stopPropagation(); // Stop event propagation here
+        onClose(); // Close the first modal
+        onOpenSecondModal(); // Open the second modal
+    };
+
+    const ButtonsContainer = (
+        <Box sx={{ display: 'flex', alignItems: 'center', width:'100%', padding: '0px 8px', gap:'16px', justifyContent: 'space-between' }}>
+            <Button variant="contained" endIcon={<CancelOutlined />} sx={{ bgcolor: '#D32F2F', color: '#fff', borderRadius: '12px', padding: '14px 24px', transition: 'background-color 0.3s', fontSize: '14px', ':hover': { bgcolor: '#B71C1C' } }} 
+                onClick={onClose}>
+                Batalkan
+            </Button>
+            <Button variant="contained" endIcon={<CreateOutlined />} sx={{ bgcolor: '#1C2D5A', color: '#fff', borderRadius: '12px', padding: '14px 24px', transition: 'background-color 0.3s', fontSize: '14px', ':hover': { bgcolor: '#122350' } }}
+                onClick={handleUbahMatrixButtonClick}>
+                Ubah Matrix
+            </Button>
+        </Box>
+    );
+
+    const DividerContainer = (
+        <Box sx={{ width: '100%', textAlign: 'center', backgroundColor: '#E0E0E0' }}>
+            <Divider orientation="horizontal" flexItem /> 
+        </Box>
+    );
+
+    return (
+        <Dialog open={open} onClose={onClose}>
+            <DialogTitle>
+                <Typography sx={{ fontSize: '24px', fontWeight: '700', textAlign:'center', marginTop: '10px' }}>Ubah Kategori Matrix</Typography>
+            </DialogTitle>
+            {DividerContainer}
+            <DialogContent>
+                <Box>
+                    <TextField
+                        select
+                        required
+                        id="kategori-matrix-akhir"
+                        label="Kategori Matrix"
+                        sx={{ width: '100%' }}
+                    >
+                        <MenuItem value="1">High Potential</MenuItem>
+                        <MenuItem value="2">Promotable-4</MenuItem>
+                        <MenuItem value="3">Prommotable-3</MenuItem>
+                        {/* Add more options if needed */}
+                    </TextField>
+                    <Typography variant="body1" sx={{ width: '90%', margin: 'auto', marginTop: '24px', marginBottom: '24px', fontFamily: 'Roboto', fontSize: '14px', fontWeight: 400, lineHeight: '20px', letterSpacing: '0.5px', textAlign: 'center' }}>
+                        Perubahan hanya diperbolehkan naik 1 level atau turun 1 level dari kategori matrix yang sudah ditentukan saat BPJ.
+                    </Typography>
+                </Box>
+            </DialogContent>
+            <DialogActions sx={{ padding:'0 24px 24px 24px ' }}>
+                {ButtonsContainer}
+            </DialogActions>
+
+            <KonfirmasiUbahMatrix 
+                open={openKonfirmasiModal} 
+                onClose={handleCloseKonfirmasiModal} />
+
+        </Dialog>
+    );
+}
+
+export default UbahKategoriMatrix;
--- src/ui-component/tables/talentclusterketuakomitetalent.jsx
+++ src/ui-component/tables/talentclusterketuakomitetalent.jsx
@@ -1,16 +1,18 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { CreateOutlined } from '@mui/icons-material';
 import { Button } from '@mui/material';
+import UbahKategoriMatrix from '../../ui-component/modal/ubah-kategori-matrix';
+import KonfirmasiUbahMatrix from '../../ui-component/modal/konfirmasi-ubah-matrix';
 
 const getStyledRenderCell = () => (params) => (
     <div>
-      <span style={{ 
-        color: '#66BB6A', 
-        backgroundColor: params.value ? '#F5FFF5' : 'transparent', 
-        padding: '4px 8px', 
-        borderRadius: '24px' 
-      }}>{params.value}</span>
+        <span style={{ 
+            color: '#66BB6A', 
+            backgroundColor: params.value ? '#F5FFF5' : 'transparent', 
+            padding: '4px 8px', 
+            borderRadius: '24px' 
+        }}>{params.value}</span>
     </div>
 );
 
@@ -38,125 +40,156 @@ const getStatusStyle = (status) => {
 
 const getKategoriMatrixStyle = () => (params) => (
     <div>
-      <span style={{ 
-        color: '#2196F3', 
-        backgroundColor: params.value ? '#EAF8FF' : 'transparent', 
-        padding: '4px 8px', 
-        borderRadius: '24px' 
-      }}>{params.value}</span>
+        <span style={{ 
+            color: '#2196F3', 
+            backgroundColor: params.value ? '#EAF8FF' : 'transparent', 
+            padding: '4px 8px', 
+            borderRadius: '24px' 
+        }}>{params.value}</span>
     </div>
 );
-  
-// Define columns with the reusable renderCell function
-const columns = [
-    { field: 'id', headerName: 'No', width: 70 },
-    { field: 'nama', headerName: 'Nama', width: 130 },
-    { field: 'nippos', headerName: 'NIPPOS', width: 130 },
-    { field: 'posisi', headerName: 'Posisi', width: 130 },
-    { field: 'joblevel', headerName: 'Job Level', width: 130 },
-    { field: 'jobfam', headerName: 'Rumpun Jabatan', width: 130 },
-    { field: 'kantor', headerName: 'Kantor', width: 130 },
-    { field: 'komiteunit', headerName: 'Komite Unit', width: 130 },
-    { field: 'competency', headerName: 'Competency/Psychotest', width: 200, renderCell: getStyledRenderCell() },
-    { field: 'pms', headerName: 'PMS', width: 130, renderCell: getStyledRenderCell() },
-    { field: 'akhlak', headerName: 'AKHLAK', width: 130, renderCell: getStyledRenderCell() },
-    { field: 'learningagility', headerName: 'Learning Agility', width: 180, renderCell: getStyledRenderCell() },
-    { field: 'avgtalentdays', headerName: 'Nilai Avg Talent Days', width: 200, renderCell: getStyledRenderCell() },
-    {
-        field: 'status',
-        headerName: 'Status',
-        width: 130,
-        renderCell: (params) => {
-            const { color, backgroundColor } = getStatusStyle(params.value);
-            return (
-            <div>
-                <span style={{ 
-                color,
-                backgroundColor,
-                padding: '4px 8px',
-                borderRadius: '24px' 
-                }}>{params.value}</span>
-            </div>
-            );
+
+const TalentClusterKetuaKomiteTalentTable = () => {
+    const [openFirstModal, setOpenFirstModal] = useState(false);
+    const [openSecondModal, setOpenSecondModal] = useState(false);
+
+    const handleOpenFirstModal = () => {
+        setOpenFirstModal(true); 
+    };
+
+    const handleCloseFirstModal = () => {
+        setOpenFirstModal(false); 
+    };
+
+    const handleOpenSecondModal = () => {
+        setOpenSecondModal(true);
+    };
+
+    const handleCloseSecondModal = () => {
+        setOpenSecondModal(false);
+    };
+
+    const columns = [
+        { field: 'id', headerName: 'No', width: 70 },
+        { field: 'nama', headerName: 'Nama', width: 130 },
+        { field: 'nippos', headerName: 'NIPPOS', width: 130 },
+        { field: 'posisi', headerName: 'Posisi', width: 130 },
+        { field: 'joblevel', headerName: 'Job Level', width: 130 },
+        { field: 'jobfam', headerName: 'Rumpun Jabatan', width: 130 },
+        { field: 'kantor', headerName: 'Kantor', width: 130 },
+        { field: 'komiteunit', headerName: 'Komite Unit', width: 130 },
+        { field: 'competency', headerName: 'Competency/Psychotest', width: 200, renderCell: getStyledRenderCell() },
+        { field: 'pms', headerName: 'PMS', width: 130, renderCell: getStyledRenderCell() },
+        { field: 'akhlak', headerName: 'AKHLAK', width: 130, renderCell: getStyledRenderCell() },
+        { field: 'learningagility', headerName: 'Learning Agility', width: 180, renderCell: getStyledRenderCell() },
+        { field: 'avgtalentdays', headerName: 'Nilai Avg Talent Days', width: 200, renderCell: getStyledRenderCell() },
+        {
+            field: 'status',
+            headerName: 'Status',
+            width: 130,
+            renderCell: (params) => {
+                const { color, backgroundColor } = getStatusStyle(params.value);
+                return (
+                    <div>
+                        <span style={{ 
+                            color,
+                            backgroundColor,
+                            padding: '4px 8px',
+                            borderRadius: '24px' 
+                        }}>{params.value}</span>
+                    </div>
+                );
+            },
         },
-    },
-    { field: 'kategorimatrix', headerName: 'Kategori Matrix', width: 180, renderCell: getKategoriMatrixStyle() },
-    {
-        field: 'aksi',
-        headerName: 'Aksi',
-        width: 180,
-        renderCell: (params) => {
-          return (
-            <Button 
-              variant="contained" 
-              sx={{
-                backgroundColor:'#1C2D5A', 
-                color: '#FFFFFF',
-                borderRadius:'12px', 
-                padding: '6px 16px'
-              }} 
-              endIcon={<CreateOutlined />}
-            >
-              Ubah Matrix
-            </Button>
-          );
+        { field: 'kategorimatrix', headerName: 'Kategori Matrix', width: 180, renderCell: getKategoriMatrixStyle() },
+        {
+            field: 'aksi',
+            headerName: 'Aksi',
+            width: 180,
+            renderCell: (params) => {
+                return (
+                    <Button 
+                        variant="contained" 
+                        sx={{
+                            backgroundColor:'#1C2D5A', 
+                            color: '#FFFFFF',
+                            borderRadius:'12px', 
+                            padding: '6px 16px'
+                        }} 
+                        endIcon={<CreateOutlined />}
+                        onClick={handleOpenFirstModal}
+                    >
+                        Ubah Matrix
+                    </Button>
+                );
+            },
         },
-    },
+    ];
 
-];
+    const rows = [
+        { id: 1, nama: 'Sri Hartini', nippos: '998494379', posisi: 'Asisten Manajer Pengembangan Join Operation',
+        joblevel: 'D3', jobfam: 'Bisnis', kantor: 'Kantor Pusat Bandung', komiteunit: 'ABD HAFID',
+        competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Turun',
+        kategorimatrix: 'Promotable-4' },
+        { id: 2, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
+        joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
+        competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Naik',
+        kategorimatrix: 'Promotable-4' },
+        { id: 3, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
+        joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
+        competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Tetap',
+        kategorimatrix: 'High Potential' },
+        { id: 4, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
+        joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
+        competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Turun' ,
+        kategorimatrix: 'Promotable-4' },
+        { id: 5, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
+        joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
+        competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Naik' ,
+        kategorimatrix: 'Promotable-4' },
+        { id: 6, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
+        joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
+        competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Tetap' ,
+        kategorimatrix: 'High Potential' },
+        { id: 7, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
+        joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
+        competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Turun' ,
+        kategorimatrix: 'Promotable-4' },
+        { id: 8, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
+        joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
+        competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Naik' ,
+        kategorimatrix: 'Promotable-4' },
+        { id: 9, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
+        joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
+        competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Tetap' ,
+        kategorimatrix: 'High Potential' },
+    ];
 
-const rows = [
-    { id: 1, nama: 'Sri Hartini', nippos: '998494379', posisi: 'Asisten Manajer Pengembangan Join Operation',
-    joblevel: 'D3', jobfam: 'Bisnis', kantor: 'Kantor Pusat Bandung', komiteunit: 'ABD HAFID',
-    competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Turun',
-    kategorimatrix: 'Promotable-4' },
-    { id: 2, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
-    joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
-    competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Naik',
-    kategorimatrix: 'Promotable-4' },
-    { id: 3, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
-    joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
-    competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Tetap',
-    kategorimatrix: 'High Potential' },
-    { id: 4, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
-    joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
-    competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Turun' ,
-    kategorimatrix: 'Promotable-4' },
-    { id: 5, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
-    joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
-    competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Naik' ,
-    kategorimatrix: 'Promotable-4' },
-    { id: 6, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
-    joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
-    competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Tetap' ,
-    kategorimatrix: 'High Potential' },
-    { id: 7, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
-    joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
-    competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Turun' ,
-    kategorimatrix: 'Promotable-4' },
-    { id: 8, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
-    joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
-    competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Naik' ,
-    kategorimatrix: 'Promotable-4' },
-    { id: 9, nama : 'Muhamad Arsyi', nippos:'999494379', posisi :'Asisten Manajer Acquisition Biller', 
-    joblevel:'D3', jobfam :'Bisnis', kantor:'Kantor Pusat Bandung', komiteunit:'ABDU SOMAD',
-    competency: 3.5, pms: 88, akhlak: 4.9, learningagility: 4.9, avgtalentdays: 88, status: 'Tetap' ,
-    kategorimatrix: 'High Potential' },
-];
+    return (
+        <div style={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={{
+                    pagination: {
+                        paginationModel: { page: 0, pageSize: 5 },
+                    },
+                }}
+                pageSizeOptions={[5, 10]}
+            />
+            
+            <UbahKategoriMatrix 
+                open={openFirstModal} 
+                onClose={handleCloseFirstModal} 
+                onOpenSecondModal={handleOpenSecondModal} />
 
-export default function TalentClusterKetuaKomiteTalentTable() {
-  return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-      />
-    </div>
-  );
-}
+            <KonfirmasiUbahMatrix 
+                open={openSecondModal} 
+                onClose={handleCloseSecondModal} 
+                onConfirm={handleCloseSecondModal} />
+
+        </div>
+    );
+};
+
+export default TalentClusterKetuaKomiteTalentTable;
