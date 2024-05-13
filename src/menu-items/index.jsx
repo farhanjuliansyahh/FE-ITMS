7c610c3cc126e011168f83b08941c228df2b35de
Doni Syahtria <mdonisyahtria@gmail.com>
2024-05-13 11:20:31 +0700

    fix login
    yang sebelumnya masih ditutup karena ada issue

--- src/menu-items/index.jsx
+++ src/menu-items/index.jsx
@@ -5,36 +5,40 @@ import ManajemenPengguna from './manajemen-pengguna';
 import EventKaryawan from './event-karyawan';
 import EventKetuaKomiteTalent from './event-ketuakomitetalent';
 
-const filterMenuItems = (role) => {
-  switch (role) {
-    case 'Admin':
-      return {
-        items: [dashboard, TalentIdentification, ManajemenPengguna]
-      };
-    case 'Karyawan':
-      return {
-        items: [EventKaryawan]
-      };
-    case 'Komite Unit':
-      return {
-        items: [EventKomiteUnit]
-      };
-    case 'Ketua Komite Talent':
-      return {
-        items: [EventKetuaKomiteTalent]
-      };
-    default:
-      return {
-        items: [dashboard]
-      };
-  }
+// const filterMenuItems = (role) => {
+//   switch (role) {
+//     case 'Admin':
+//       return {
+//         items: [dashboard, TalentIdentification, ManajemenPengguna]
+//       };
+//     case 'Karyawan':
+//       return {
+//         items: [EventKaryawan]
+//       };
+//     case 'Komite Unit':
+//       return {
+//         items: [EventKomiteUnit]
+//       };
+//     case 'Ketua Komite Talent':
+//       return {
+//         items: [EventKetuaKomiteTalent]
+//       };
+//     default:
+//       return {
+//         items: [dashboard]
+//       };
+//   }
+// };
+
+// const roleString = sessionStorage.getItem('role');
+// const role = roleString ? JSON.parse(roleString)[0] : null;
+
+// console.log('inirole', role);
+
+// const menuItems = filterMenuItems(role);
+
+const menuItems = {
+  items: [dashboard, TalentIdentification, EventKetuaKomiteTalent, EventKomiteUnit, EventKaryawan, ManajemenPengguna]
 };
-
-const roleString = sessionStorage.getItem('role');
-const role = roleString ? JSON.parse(roleString)[0] : null;
-
-console.log('inirole', role);
-
-const menuItems = filterMenuItems(role);
 
 export default menuItems;
