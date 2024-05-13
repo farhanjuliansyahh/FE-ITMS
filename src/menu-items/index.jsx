103e732aff18f3d2a3ad10847bdf044d1c0977c5
Doni Syahtria <mdonisyahtria@gmail.com>
2024-05-13 09:10:07 +0700

    perbaikan login
    masih terdapat bug yang belum diselesaikan

--- src/menu-items/index.jsx
+++ src/menu-items/index.jsx
@@ -5,17 +5,36 @@ import ManajemenPengguna from './manajemen-pengguna';
 import EventKaryawan from './event-karyawan';
 import EventKetuaKomiteTalent from './event-ketuakomitetalent';
 
-// ==============================|| MENU ITEMS ||============================== //
-
-const menuItems = {
-  items: [
-    dashboard, 
-    TalentIdentification, 
-    EventKetuaKomiteTalent,
-    EventKomiteUnit,  
-    EventKaryawan, 
-    ManajemenPengguna
-  ]
+const filterMenuItems = (role) => {
+  switch (role) {
+    case 'Admin':
+      return {
+        items: [dashboard, TalentIdentification, ManajemenPengguna]
+      };
+    case 'Karyawan':
+      return {
+        items: [EventKaryawan]
+      };
+    case 'Komite Unit':
+      return {
+        items: [EventKomiteUnit]
+      };
+    case 'Ketua Komite Talent':
+      return {
+        items: [EventKetuaKomiteTalent]
+      };
+    default:
+      return {
+        items: [dashboard]
+      };
+  }
 };
+
+const roleString = sessionStorage.getItem('role');
+const role = JSON.parse(roleString)[0]; // Parse the string back to an array and get the first element
+
+console.log('inirole', role);
+
+const menuItems = filterMenuItems(role);
 
 export default menuItems;
