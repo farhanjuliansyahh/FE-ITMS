bb554ec0c9e2618dc41938a3cfc919b190bb24db
Doni Syahtria <mdonisyahtria@gmail.com>
2024-05-16 16:15:19 +0700

    update sore

--- src/views/pages/dashboard/talent-detail.jsx
+++ src/views/pages/dashboard/talent-detail.jsx
@@ -16,21 +16,20 @@ export default function TalentDetail() {
     setLoading(false);
   }, []);
 
-  const [rows, setrows] = useState([])
+  const [rows, setRows] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:4000/getdetailtalent")
-    .then(response => response.json())
-        // 4. Setting *dogImage* to the image url that we received from the response above
-    .then(data => setrows(data))
-  },[])
+    fetch('http://localhost:4000/getdetailtalent')
+      .then((response) => response.json())
+      // 4. Setting *dogImage* to the image url that we received from the response above
+      .then((data) => setRows(data));
+  }, []);
 
-  
   // create list of Nama, Nippos, Job Level, Rumpun Jabatan
-  const listNama = [...new Set(rows.map(row => row.nama))];
-  const listJobLevel = [...new Set(rows.map(row => row.joblevel))];
-  const listRumpunJabatan = [...new Set(rows.map(row => row.jobfam))];
-  const listKantor = [...new Set(rows.map(row => row.nama_kantor))];
+  const listNama = [...new Set(rows.map((row) => row.nama))];
+  const listJobLevel = [...new Set(rows.map((row) => row.joblevel))];
+  const listRumpunJabatan = [...new Set(rows.map((row) => row.jobfam))];
+  const listKantor = [...new Set(rows.map((row) => row.nama_kantor))];
 
   const [selectedNama, setSelectedNama] = useState(null);
   const [selectedJobLevel, setSelectedJobLevel] = useState(null);
@@ -64,26 +63,52 @@ export default function TalentDetail() {
     resetJobLevelInput();
     resetRumpunJabatanInput();
     resetKantorInput();
-
   };
   //Unduh Button Action
   const handleDownloadCSV = () => {
     // Create a CSV header with column names
-    const headers = Object.keys(rows[0]);
+    const headers = Object.keys(resetRows[0]);
     const headerRow = headers.join(',');
-  
+
     // Convert data to CSV format
-    const csvContent = "data:text/csv;charset=utf-8," + encodeURIComponent(headerRow + '\n' +
-      rows.map(row => headers.map(header => row[header]).join(',')).join('\n'));
-  
+    const csvContent =
+      'data:text/csv;charset=utf-8,' +
+      encodeURIComponent(headerRow + '\n' + resetRows.map((row) => headers.map((header) => row[header]).join(',')).join('\n'));
+
     // Create a temporary anchor element
     const link = document.createElement('a');
     link.setAttribute('href', csvContent);
-    link.setAttribute('download', 'Daftartalent_data.csv');
-  
+    link.setAttribute('download', 'daftar_talent.csv');
+
     // Trigger the download
     link.click();
   };
+
+  const filteredRows = rows.filter((row) => {
+    const namaMatch = !selectedNama || (row.nama && row.nama.toLowerCase().includes(selectedNama.toLowerCase()));
+    const jobLevelMatch = !selectedJobLevel || (row.joblevel && row.joblevel.toLowerCase().includes(selectedJobLevel.toLowerCase()));
+    const rumpunJabatanMatch =
+      !selectedRumpunJabatan || (row.jobfam && row.jobfam.toLowerCase().includes(selectedRumpunJabatan.toLowerCase()));
+    const kantorMatch = !selectedKantor || (row.nama_kantor && row.nama_kantor.toLowerCase().includes(selectedKantor.toLowerCase()));
+
+    return (
+      (!selectedNama || namaMatch) &&
+      (!selectedJobLevel || jobLevelMatch) &&
+      (!selectedRumpunJabatan || rumpunJabatanMatch) &&
+      (!selectedKantor || kantorMatch)
+    );
+  });
+
+  //fungsi buat reset index:
+  const resetRowIndex = (filteredRows) => {
+    return filteredRows.map((row, index) => ({
+      ...row,
+      id: index + 1 // Adding 1 to start the index from 1 instead of 0
+    }));
+  };
+
+  //buat constanta yang isinya hasil filter yang indexnya udah di reset:
+  const resetRows = resetRowIndex(filteredRows);
 
   return (
     <>
@@ -92,43 +117,50 @@ export default function TalentDetail() {
         <Grid item xs={12}>
           <Grid container spacing={gridSpacing}>
             <Grid item xs={12}>
-              <Header title={'Monitoring Talent'}/>
+              <Header title={'Monitoring Talent'} />
             </Grid>
           </Grid>
         </Grid>
 
-        
         <Grid item xs={12}>
-        <MainCard style={{ padding: '24px 24px'}} >
-            <div style={{ display: 'flex', justifyContent: 'flex-end', paddingBottom: '16px', width:'100%' }}>
-              <ButtonPrimary Color="#ffffff" icon={FileDownloadOutlined} LabelName={'Unduh Data'} onClick={handleDownloadCSV}/>
+          <MainCard style={{ padding: '24px 24px' }}>
+            <div style={{ display: 'flex', justifyContent: 'flex-end', paddingBottom: '16px', width: '100%' }}>
+              <ButtonPrimary Color="#ffffff" icon={FileDownloadOutlined} LabelName={'Unduh Data'} onClick={handleDownloadCSV} />
             </div>
-            <div style={{ display: 'flex', justifyContent: 'flex-end', paddingBottom: '16px', width:'100%' }}>
+            <div style={{ display: 'flex', justifyContent: 'flex-end', paddingBottom: '16px', width: '100%' }}>
               <Stack direction="row" spacing={2} marginRight={2} width={'100%'}>
                 <CustomSearch field={listNama} label={'Nama'} onSearch={setSelectedNama} value={selectedNama} resetInput={resetNamaInput} />
-                <CustomSearch field={listJobLevel} label={'Job Level'} onSearch={setSelectedJobLevel} value={selectedJobLevel} resetInput={resetJobLevelInput} />
-                <CustomSearch field={listRumpunJabatan} label={'Rumpun Jabatan'} onSearch={setSelectedRumpunJabatan} value={selectedRumpunJabatan} resetInput={resetRumpunJabatanInput} />
-                <CustomSearch field={listKantor} label={'Kantor'} onSearch={setSelectedKantor} value={selectedKantor} resetInput={resetKantorInput} />
+                <CustomSearch
+                  field={listJobLevel}
+                  label={'Job Level'}
+                  onSearch={setSelectedJobLevel}
+                  value={selectedJobLevel}
+                  resetInput={resetJobLevelInput}
+                />
+                <CustomSearch
+                  field={listRumpunJabatan}
+                  label={'Rumpun Jabatan'}
+                  onSearch={setSelectedRumpunJabatan}
+                  value={selectedRumpunJabatan}
+                  resetInput={resetRumpunJabatanInput}
+                />
+                <CustomSearch
+                  field={listKantor}
+                  label={'Kantor'}
+                  onSearch={setSelectedKantor}
+                  value={selectedKantor}
+                  resetInput={resetKantorInput}
+                />
               </Stack>
-              <ButtonErrorOutlined onClick={handleResetSearch} Color="#D32F2F" icon={RestartAltOutlined} LabelName={'Reset'}/>
+              <ButtonErrorOutlined onClick={handleResetSearch} Color="#D32F2F" icon={RestartAltOutlined} LabelName={'Reset'} />
             </div>
 
-            <Grid style={{marginBottom: '0.5%'}}>
-               <DetailTalentTable 
-                rows={rows}
-                searchNama={selectedNama} // Pass selectedNama as searchTerm to the NilaiAssessmentTable component
-                searchJobLevel={selectedJobLevel}
-                searchRumpunJabatan={selectedRumpunJabatan}
-                searchKantor={selectedKantor}
-               />
+            <Grid style={{ marginBottom: '0.5%' }}>
+              <DetailTalentTable filteredRows={resetRows} />
             </Grid>
-        </MainCard>
+          </MainCard>
         </Grid>
-
-
       </Grid>
     </>
   );
-};
-
-
+}
