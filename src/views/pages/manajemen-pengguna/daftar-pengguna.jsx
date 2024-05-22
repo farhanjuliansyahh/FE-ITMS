f019b8b85dbe615e666a3aa8a1597655124e7ac2
Doni Syahtria <mdonisyahtria@gmail.com>
2024-05-22 15:11:14 +0700

    update untuk hcbp

--- src/views/pages/manajemen-pengguna/daftar-pengguna.jsx
+++ src/views/pages/manajemen-pengguna/daftar-pengguna.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useCallback } from 'react';
 import { gridSpacing } from '../../../store/constant';
-import { Grid, Stack,Typography, Box} from '@mui/material';
+import { Grid, Stack, Typography, Box } from '@mui/material';
 import { FileDownloadOutlined, RestartAltOutlined } from '@mui/icons-material';
 
 import MainCard from '../../../ui-component/cards/MainCard';
@@ -13,7 +13,7 @@ import ButtonErrorOutlined from '../../../ui-component/button/ButtonErrorOutline
 
 const DaftarPengguna = () => {
   const [isLoading, setLoading] = useState(true);
-  const [rowsUser, setRowsUser] = useState([])
+  const [rowsUser, setRowsUser] = useState([]);
 
   useEffect(() => {
     setLoading(false);
@@ -22,51 +22,51 @@ const DaftarPengguna = () => {
   useEffect(() => {
     // Fetch data from API
     fetch(`http://localhost:4000/getallroles`)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         // Update state with API data
-        setRowsUser(data.map((row, index) => ({ ...row, id: index + 1 })))
+        setRowsUser(data.map((row, index) => ({ ...row, id: index + 1 })));
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error fetching data:', error);
       });
   }, []);
 
-    const userLength = rowsUser.length
+  const userLength = rowsUser.length;
 
-    //
-    const listNama = [...new Set(rowsUser.map(row => row.nama))];
-    const listNippos = [...new Set(rowsUser.map(row => row.nippos))];
-    const listPeran = [...new Set(rowsUser.map(row => row.Peran))];
+  //
+  const listNama = [...new Set(rowsUser.map((row) => row.nama))];
+  const listNippos = [...new Set(rowsUser.map((row) => row.nippos))];
+  const listPeran = [...new Set(rowsUser.map((row) => row.Peran))];
 
-    const [selectedNama, setSelectedNama] = useState(null);
-    const [selectedNippos, setSelectedNippos] = useState(null);
-    const [selectedPeran, setSelectedPeran] = useState(null);
-  
-    const resetNamaInput = () => {
-      setSelectedNama('');
-    };
-  
-    const resetNipposInput = () => {
-      setSelectedNippos('');
-    };
-  
-    const resetPeranInput = () => {
-      setSelectedPeran('');
-    };
-  
-    const handleResetSearch = () => {
-      setSelectedNama('');
-      setSelectedNippos('');
-      setSelectedPeran('');
-  
-      // Call resetInput function for each CustomSearch component
-      resetNamaInput();
-      resetNipposInput();
-      resetPeranInput();
-    };
-  
-    const [filteredData, setFilteredData] = useState([]); // State to hold the filtered data
+  const [selectedNama, setSelectedNama] = useState(null);
+  const [selectedNippos, setSelectedNippos] = useState(null);
+  const [selectedPeran, setSelectedPeran] = useState(null);
+
+  const resetNamaInput = () => {
+    setSelectedNama('');
+  };
+
+  const resetNipposInput = () => {
+    setSelectedNippos('');
+  };
+
+  const resetPeranInput = () => {
+    setSelectedPeran('');
+  };
+
+  const handleResetSearch = () => {
+    setSelectedNama('');
+    setSelectedNippos('');
+    setSelectedPeran('');
+
+    // Call resetInput function for each CustomSearch component
+    resetNamaInput();
+    resetNipposInput();
+    resetPeranInput();
+  };
+
+  const [filteredData, setFilteredData] = useState([]); // State to hold the filtered data
 
   // Memoize the function to prevent infinite loops
   const handleFilteredData = useCallback((data) => {
@@ -81,7 +81,7 @@ const DaftarPengguna = () => {
   const handleDownloadCSV = () => {
     let dataToDownload = [];
     let filename = '';
-    
+
     dataToDownload = filteredData;
     filename = `Daftar_Pengguna.csv`;
 
@@ -93,21 +93,24 @@ const DaftarPengguna = () => {
       headers.unshift('id'); // Insert 'id' at the beginning
     }
     const headerRow = headers.join(',');
-  
+
     // Convert data to CSV format
-    const csvContent = "data:text/csv;charset=utf-8," + headerRow + '\n' +
-      dataToDownload.map(row => headers.map(header => row[header]).join(',')).join('\n');
-  
+    const csvContent =
+      'data:text/csv;charset=utf-8,' +
+      headerRow +
+      '\n' +
+      dataToDownload.map((row) => headers.map((header) => row[header]).join(',')).join('\n');
+
     // Create a temporary anchor element
     const encodedUri = encodeURI(csvContent);
-    const link = document.createElement("a");
-    link.setAttribute("href", encodedUri);
-    link.setAttribute("download", filename);
+    const link = document.createElement('a');
+    link.setAttribute('href', encodedUri);
+    link.setAttribute('download', filename);
     document.body.appendChild(link);
-  
+
     // Trigger the download
     link.click();
-  
+
     // Clean up
     document.body.removeChild(link);
   };
@@ -116,55 +119,74 @@ const DaftarPengguna = () => {
     <>
       {/* <MainLayout/> */}
       <MainCard>
-        <Grid container spacing={gridSpacing} padding={'24px'} >
+        <Grid container spacing={gridSpacing} padding={'24px'}>
           <Grid item xs={12}>
             <Grid container spacing={gridSpacing}>
               <Grid item xs={12}>
-              <Box style={{ width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-                <Stack direction="row" spacing={2} alignItems="center"  style={{marginBottom: '16px'}}>
-                  <Typography style={{display: 'inline',fontFamily: 'Roboto', fontSize:'24px', fontWeight: '700' }} gutterBottom>
-                    Tabel Karyawan
-                  </Typography>
-                  <Typography variant="body2" 
-                    style={{
-                      color:'#2196F3',
-                      backgroundColor:'#EAF8FF', // Default background color
-                      padding: '8px 16px', // Adjust padding as needed
-                      borderRadius: '16px', // Adjust border radius for rounded corners
-                      display: 'inline-block', // Ensure inline display
-                      fontSize: '16px',
-                      fontFamily:'Roboto',
-                      fontWeight: 500
-                    }}>
-                    {userLength} Talent Karyawan
-                  </Typography>
-                  <Box sx={{ flexGrow: 1 }} /> {/* This will push the following elements to the right */}
-                  <Stack direction="row" spacing={1}>
-                    <ButtonPrimary Color="#ffffff" icon={FileDownloadOutlined} LabelName={'Unduh Data'} onClick={handleDownloadCSV}/>
+                <Box style={{ width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                  <Stack direction="row" spacing={2} alignItems="center" style={{ marginBottom: '16px' }}>
+                    <Typography style={{ display: 'inline', fontFamily: 'Roboto', fontSize: '24px', fontWeight: '700' }} gutterBottom>
+                      Tabel Karyawan
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      style={{
+                        color: '#2196F3',
+                        backgroundColor: '#EAF8FF', // Default background color
+                        padding: '8px 16px', // Adjust padding as needed
+                        borderRadius: '16px', // Adjust border radius for rounded corners
+                        display: 'inline-block', // Ensure inline display
+                        fontSize: '16px',
+                        fontFamily: 'Roboto',
+                        fontWeight: 500
+                      }}
+                    >
+                      {userLength} Talent Karyawan
+                    </Typography>
+                    <Box sx={{ flexGrow: 1 }} /> {/* This will push the following elements to the right */}
+                    <Stack direction="row" spacing={1}>
+                      <ButtonPrimary Color="#ffffff" icon={FileDownloadOutlined} LabelName={'Unduh Data'} onClick={handleDownloadCSV} />
+                    </Stack>
                   </Stack>
-                </Stack>
 
-                <div style={{ display: 'flex', justifyContent: 'flex-end', paddingBottom: '16px', width:'100%' }}>
-                  <Stack direction="row" spacing={2} marginRight={2} width={'100%'}>
-                    <CustomSearch field={listNama} label={'Nama'} onSearch={setSelectedNama} value={selectedNama} resetInput={resetNamaInput} />
-                    <CustomSearch field={listNippos} label={'Nippos'} onSearch={setSelectedNippos} value={selectedNippos} resetInput={resetNipposInput} />
-                    <CustomSearch field={listPeran} label={'Peran'} onSearch={setSelectedPeran} value={selectedPeran} resetInput={resetPeranInput} />
-                  </Stack>
-                  <ButtonErrorOutlined onClick={handleResetSearch} Color="#D32F2F" icon={RestartAltOutlined} LabelName={'Reset'}/>
-                </div>
+                  <div style={{ display: 'flex', justifyContent: 'flex-end', paddingBottom: '16px', width: '100%' }}>
+                    <Stack direction="row" spacing={2} marginRight={2} width={'100%'}>
+                      <CustomSearch
+                        field={listNama}
+                        label={'Nama'}
+                        onSearch={setSelectedNama}
+                        value={selectedNama}
+                        resetInput={resetNamaInput}
+                      />
+                      <CustomSearch
+                        field={listNippos}
+                        label={'Nippos'}
+                        onSearch={setSelectedNippos}
+                        value={selectedNippos}
+                        resetInput={resetNipposInput}
+                      />
+                      <CustomSearch
+                        field={listPeran}
+                        label={'Peran'}
+                        onSearch={setSelectedPeran}
+                        value={selectedPeran}
+                        resetInput={resetPeranInput}
+                      />
+                    </Stack>
+                    <ButtonErrorOutlined onClick={handleResetSearch} Color="#D32F2F" icon={RestartAltOutlined} LabelName={'Reset'} />
+                  </div>
 
-                <DaftarPenggunaTabel 
-                  rows={rowsUser}
-                  searchNama={selectedNama} 
-                  searchNippos={selectedNippos}
-                  searchPeran={selectedPeran}
-                  onFilteredData={handleFilteredData} // Pass the function to child component
-                />
-              </Box>
+                  <DaftarPenggunaTabel
+                    rows={rowsUser}
+                    searchNama={selectedNama}
+                    searchNippos={selectedNippos}
+                    searchPeran={selectedPeran}
+                    onFilteredData={handleFilteredData} // Pass the function to child component
+                  />
+                </Box>
               </Grid>
             </Grid>
           </Grid>
-          
         </Grid>
       </MainCard>
     </>
